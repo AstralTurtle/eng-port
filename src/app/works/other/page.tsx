e127b8b1b8ce1423fde8738dbb98eacf8f4b65ca
Aaron J <jamesacs329@gmail.com>
2024-12-15 14:54:29 -0500

    add other

--- src/app/works/other/page.tsx
+++ src/app/works/other/page.tsx
@@ -1,5 +1,45 @@
 export default function otherWork(){
     return(
-        <h1>Check back later!</h1>
+        <main>
+            <h1 className="text-3xl text-center text-bold">Assorted Works</h1>
+        <div className="p-3">
+            <div>
+                <h3 className="font-bold">Start a story with:
+“It was the weirdest thing I had ever seen in my life…” and continue from there
+</h3>
+                <p>It was the weirdest thing I had seen in my life- a purple barked tree  growing neon blue  apples. I mean, the tree kinda just appeared out of my window one night. I was going to go yell at whoever was shining that light into my room and then bam, neon blue apples.</p>
+            </div>
+            <br />
+            <div>
+                <h3 className="font-bold">Write from the perspective of somebody who is not you, in the first-person.
+Choose anyone. Get as creative as you want. Someone real or not real, can be someone famous or someone made up, or someone you know in your life.
+ANYONE who is not you. <span className="text-green-300"> (Written from the perspective of my DnD Character, A Fairy Wizard)</span>
+</h3>
+                <p>This dungeons sucks. I've been in here for less then an hour, and I have died in a fire, fallen into a pit despite my wings, and gotten attacked by skulls. Flying skulls. Not to mention just before this I had to deal with adventurers and their stereotypes. Really, a hivemind- oghma forgive that the magically inclined race have a brain. I'm all out of spells at this point, and have definitely had enough excite for a tenday. Many tendays. I think Galahmo was right, I was being to hasty adventuring, but this fairy doesn't know how to give up. (I certainly can learn though).</p>
+            </div>
+            <br />
+            <div>
+                <h3 className="font-bold">Write about a strange weather event or occurrence. What did you think/feel/do during that occurrence? What was strange or unusual about it?</h3>
+                <p>I remember the orange smoke at the end of junior year, But I also remember the earthquake in new jersey that we felt in NY.  More specifically I didn't feel the earthquake because I was in my schools basement, but I read about it and panic called my mom, and then panic texted my aunt who is in jersey who did feel it. I also remember it because I had a job interview I had to be home for and I was worried it would effect the buses and me getting home.</p>
+            </div>
+            <br />
+            <div>
+                <h3 className="font-bold">Consider the last product you encountered that excited you (in any way). Was it a new video game? Was it your new iphone? Was it a specific car model you saw on the street? Why did it excite you?</h3>
+                <p>I've been subtly excited for windblown for about a year, but with it's release slated for this month i'm now overtly excited about it. It's a release by a studio who's previous game is a cult classic (and I loved), in a similar genre that I prefer, focusing on refining a mechanic I love in a way that makes me feel like it will eat up hours of my time. The early access video I saw makes it seem like they delivered on many of their promises, and the game is going to be difficult, fast and fun.</p>
+                <br />
+            </div>
+            <div >
+                <h3 className="font-bold">Part 1: What’s something you’re an expert in? Or what is something you’re just passionate about? (A hobby, a video game, a music genre, etc.) Maybe it’s an obsession. When did your interest first start and how did you find out more about your interest? 
+                </h3> 
+                
+                <p>It's on my mind because I spent like 7 hours playing it yesterday, so i'm going to talk about Supervive, a game running an alpha test this week. It started in june or july, when the NDA around the game dropped. A league youtuber/stream who I was watching a lot released a bunch of videos around it, and I got really excited. I want to note that I had heard of the game before, and actually had access to it, but never played it during the playtests. So when the NDA dropped and the youtuber released a guide, I got really excited about the game. Additionally they had a refer-a-friend program, so I got my friends to sign up and play with me. The first playtest I played almost every day, and I got really into the game. The last time I was this into a game was League of Legends and I put almost 50 hours into that game when I was obsessed with it.</p>
+                <br /> <h3 className="font-bold">Part 2: How would you describe this to somebody who’s never heard of or encountered your passion? Or who has very little information about your passion? </h3>
+                <p>Supervive, is a MOBA/Hero Shooter/Battle Royale fusion. It consists of Squads of 20/10 teams of 2/4 in a battle royale, It plays like a MOBA, very close to battlerite and somewhat adjacent to brawl stars. It has the depth of a MOBA, with interesting choices in items, area specific mechanics to learn, and requiring knowledge of every characters kits, playstyles, and threat ranges to play well. It's not simple, but it comes together to form a cohesive gameplay loop that is fun and engaging. </p>
+                <br />
+            </div>
+        </div></main>
+        
     )
 }
+
+// div>h3.font-bold+p+br
